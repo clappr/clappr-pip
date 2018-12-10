b937c2c3aa426946cc756e52830fdc672838500f
Bruno Torres <me@brunotorr.es>
2018-12-10 19:18:24 -0200

    chore: update build/release settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,48 @@ const path = require('path')
 const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-var plugins = [
-  new CleanWebpackPlugin(['dist']),
+let plugins = [
   new webpack.DefinePlugin({
     version: JSON.stringify(require('./package.json').version),
   })
 ];
 
-module.exports = {
-  mode: 'none',
-  entry: path.resolve(__dirname, 'src/index.js'),
-  plugins: plugins,
-  externals: {
-    clappr: 'Clappr'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.svg/, loader: 'svg-inline-loader'
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'clappr-pip.js',
-    library: 'ClapprPip',
-    libraryTarget: 'umd',
-  },
-  devServer: {
-    contentBase: 'public/',
-    host: '0.0.0.0',
-    disableHostCheck: true,
-    inline: false
-  },
+module.exports = (env, argv) => {
+  if (argv.mode !== 'production') {
+    plugins.push(new CleanWebpackPlugin(['dist']));
+  }
+  return {
+    mode: argv.mode || 'none',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    plugins: plugins,
+    externals: {
+      clappr: 'Clappr'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.svg/, loader: 'svg-inline-loader'
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'clappr-pip.js',
+      library: 'ClapprPip',
+      libraryTarget: 'umd',
+    },
+    devServer: {
+      contentBase: 'public/',
+      host: '0.0.0.0',
+      disableHostCheck: true,
+      inline: false
+    },
+  };
 };
